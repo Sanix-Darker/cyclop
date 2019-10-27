7927f1635c3c636e0b1f388b59c1d8e8ee37d552
sanix-darker <s4nixd@gmail.com>
2019-10-27 22:26:58 +0100

    Update HS file and o file

--- tests/test.go
+++ tests/test.go
@@ -1,3 +1,9 @@
+// ------
+// cyclop example, to test it, just add in FILE_TO_WATCH Array(in cyclop.sh) this command :
+//
+// ./cyclop.sh f '(["./tests/test.go"]="go run ./tests/test.go")'
+// ------
+
 package main
 
 import "fmt"
