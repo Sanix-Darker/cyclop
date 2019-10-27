8ddf4b8682df6a34b8b177348558ee6aab409568
sanix-darker <s4nixd@gmail.com>
2019-10-27 08:54:33 +0100

    Changing images && README updated

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,5 @@
 // ------
-// cyclop example, to test it, just add in FILE_TO_WATCH Array(in cyclop.sh) this command :
+// Cyclope example, to test it, just add in FILE_TO_WATCH Array(in cyclope.sh) this command :
 //
 // ["./tests/test.js"]="node ./tests/test.js"
 // So it will look like this : declare -A FILE_TO_WATCH=( ["./tests/test.js"]="node ./tests/test.js" )
@@ -7,7 +7,7 @@
 
 helloWorld = () => {
     console.log("[+] Hello World on JS is amazing!");
-    console.log("[+] WOW !!")
+    console.log("[+] WOW !!!")
 }
 
 helloWorld()
