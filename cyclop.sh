543d6e3b36f8d490e4929670ffd9312487af7d1c
sanix-darker <s4nixd@gmail.com>
2019-12-12 19:41:03 +0100

    Global Updates for the shellcheck SC2086

--- cyclop.sh
+++ cyclop.sh
@@ -19,7 +19,7 @@ SEE_LOGS="0"
 logger()
 {
     if [ "$SEE_LOGS" = "1" ];then # "0" for the oppossite
-        echo $1
+        echo "$1"
     fi
 }
 
@@ -32,26 +32,27 @@ checkSum()
     value=""
     # We check the system, then we compute the md5Sum
     if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        value=$(md5sum $1)
+        value=$(md5sum "$1")
     elif [[ "$OSTYPE" == "darwin"* ]]; then
-        value=$(md5 $1)
+        value=$(md5 "$1")
     elif [[ "$OSTYPE" == "cygwin" ]]; then
         # POSIX compatibility layer and Linux environment emulation for Windows
+        # shellcheck disable=SC2086
         value=$(md5sum $1)
     elif [[ "$OSTYPE" == "msys" ]]; then
         # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-        value=$(FCIV -md5 -sha1 $1)
+        value=$(FCIV -md5 -sha1 "$1")
     elif [[ "$OSTYPE" == "win32" ]]; then
         # I'm not sure this can happen.
-        value=$(FCIV -md5 -sha1 $1)
+        value=$(FCIV -md5 -sha1 "$1")
     elif [[ "$OSTYPE" == "freebsd"* ]]; then
-        value=$(md5sum $1)
+        value=$(md5sum "$1")
     else
         # Unknown.
         echo "Unknown System"
-        value="$(md5sum $1)"
+        value="$(md5sum "$1")"
     fi
-    echo $value
+    echo "$value"
 }
 
 
@@ -62,8 +63,12 @@ array_key_exists()
 {
     local _array_name="$1"
     local _key="$2"
+    # shellcheck disable=SC2016
     local _cmd='echo ${!'$_array_name'[@]}'
-    local _array_keys=($(eval $_cmd))
+    # shellcheck disable=SC2207
+    local _array_keys=($(eval "$_cmd"))
+    # shellcheck disable=SC2155
+    # shellcheck disable=SC2145
     local _key_exists=$(echo " ${_array_keys[@]} " | grep " $_key " &>/dev/null; echo $?)
     [[ "$_key_exists" = "0" ]] && return 0 || return 1
 }
@@ -76,7 +81,9 @@ execute_cmd()
     clear
     echo "[+] ------------------------------------------------------------"
     echo "[+] CYCLOP: BASH VERSION: [${BASH_VERSION}.], OS: [${OSTYPE}], WATCHING: [$3]"
-    echo "[+] CHANGES DETECTED ON: '$2', MD5SUM-FILE: '"$(checkSum $2)"'"
+    # shellcheck disable=SC2046
+    # shellcheck disable=SC2027
+    echo "[+] CHANGES DETECTED ON: '$2', MD5SUM-FILE: '"$(checkSum "$2")"'"
     echo "[+] COMMAND: '$1'"
     IFS='&&' read -ra command_list <<< "$1"
     echo "[+] ------------------------------------------------------------"
@@ -97,21 +104,24 @@ if [ "$1" = "f" ]; then
     # We run the import of the table
     declare -A FILE_TO_WATCH=$2
 
+    # shellcheck disable=SC2145
     echo "[+] Cyclop is Watching [${!FILE_TO_WATCH[@]}]"
     sum1=""
     while true
     do
         sleep 1
+        # shellcheck disable=SC2068
         for file in ${!FILE_TO_WATCH[@]}
         do
+            # shellcheck disable=SC2086
             sum2=$(checkSum $file)
             logger "[+] $sum2"
             if [ "$sum1" != "$sum2" ];
             then
                 # We execute the command here,
                 # we pass the command and the file
-                execute_cmd "${FILE_TO_WATCH[$file]}" $file $file
-                sum1=$(checkSum $file)
+                execute_cmd "${FILE_TO_WATCH[$file]}" "$file" "$file"
+                sum1=$(checkSum "$file")
                 logger "[+] $sum1"
             fi
         done
@@ -124,10 +134,12 @@ elif [ "$1" = "e" ]; then # extension list, the logic here is to save in an asso
     EXTENSIONS_TO_WATCH=$3  # For example 'js py rb'
     COMMAND_TO_EXECUTE=$4 # For example 'echo "Cyclop works"'
 
+    # shellcheck disable=SC2145
     echo "[+] Cyclop is Watching file with extensions : [${EXTENSIONS_TO_WATCH[@]}] in ${DIRECTORY_TO_WATCH}"
     declare -A ARRAY_SUM=()
 
     # We loop all over the extension
+    # shellcheck disable=SC2068
     for ext in ${EXTENSIONS_TO_WATCH[@]}
     do
         SUB_DIR_PATCH_LIST=('*.' '*/*.' '*/*/*.' '*/*/*/*.' '*/*/*/*/*.' '*/*/*/*/*/*.' \
@@ -135,12 +147,13 @@ elif [ "$1" = "e" ]; then # extension list, the logic here is to save in an asso
         for ext_p in ${SUB_DIR_PATCH_LIST[@]}
         do
             # We loop all over the file with the extension provided
+            # shellcheck disable=SC2066
             for file_ext in "${DIRECTORY_TO_WATCH}$ext_p$ext"
             do
                 for file_ext_elt in ${file_ext}
                 do
                     if test -f "$file_ext_elt"; then
-                        if [[ "$(array_key_exists 'ARRAY_SUM' $file_ext_elt; echo $?)" = "1" ]]; then
+                        if [[ "$(array_key_exists 'ARRAY_SUM' "$file_ext_elt"; echo $?)" = "1" ]]; then
                             ARRAY_SUM[$file_ext_elt]=""
                             logger ">>>> File added to array: $file_ext_elt"
                         fi
@@ -154,6 +167,7 @@ elif [ "$1" = "e" ]; then # extension list, the logic here is to save in an asso
     while true
     do
         sleep 1
+        # shellcheck disable=SC2068
         for file_ext_elt in ${!ARRAY_SUM[@]}
         do
             file_sum=$(checkSum ${file_ext_elt})
